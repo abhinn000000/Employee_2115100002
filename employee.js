c01b3b0020307abc66a2bff740a34132703601ce
a.abhinn <abhinn000000@gmail.com>
2025-03-19 23:40:44 +0530

    [Abhinn] Add: adding UC6 files to branch UC6

--- employee.js
+++ employee.js
@@ -80,5 +80,28 @@ while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
 }
 
 empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 - Total Days: " + totalWorkingDays +
-            " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+console.log("UC5 - Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+
+// UC 6 Arrays
+
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
+//const MAX_HRS_IN_MONTH = 160;
+//const NUM_OF_WORKING_DAYS = 20;
+totalEmpHrs = 0;
+totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+       totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+
+empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
